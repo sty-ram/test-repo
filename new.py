8716a519b18f307d8fddfa21379683fb9c0f9ec8
RUPAM <apple@APPLEs-MacBook-Air.local>
2025-08-05 15:33:47 +0530

    changes in file

--- new.py
+++ new.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import numpy as np
+def create_dataframe():
+    # Create a DataFrame with 100 rows and 5 columns
+    df = pd.DataFrame(np.random.rand(100, 5), columns=list('ABCDE'))
+    
+    # Add a new column 'F' with random integers between 1 and 10
+    df['F'] = np.random.randint(1, 11, size=(100,))
+    
+    return df   
